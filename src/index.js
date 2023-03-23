b09afc5e62121981232ff3e58475dc3a915b5c37
Harel Natan <harelnatan7@gmail.com>
2023-03-23 23:00:15 +0200

    added assets and his folders

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './assets/styles/main.scss'
 import { RootCmp } from './root-cmp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
